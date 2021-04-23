149140e14b59e932aea7fd8a7d3d5b346cab579e
farhan aldiansyah <farhanpoetra011@gmail.com>
2021-04-24 02:51:35 +0700

    Membuat validasi API login dan register

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    // function login
     public function login(Request $request)
     {
-        // dd($request->all());
-        // die();
+        
         $user = User::where('email', $request->email)->first();
         // cek user kosong atau tidak
         if ($user) {
@@ -48,8 +48,10 @@ class UserController extends Controller
 
         // cek validasi
         if($validasi->fails()){
+
             $val = $validasi->errors()->all();
             return $this->error($val[0]);
+
         }
 
         // input data kedalam database
@@ -60,11 +62,13 @@ class UserController extends Controller
 
         // cek user
         if ($user) {
+
             return response()->json([
                 'success' => 1,
                 'message' => 'Selamat anda berhasil register',
                 'user' => $user
             ]);
+            
         }
 
         return $this->error('Registrasi Gagal!');
